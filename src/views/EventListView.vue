c39d7d2eb2e3b10b8a75d4928124054617cf8b42
Y <htook2660@gmail.com>
2026-07-19 15:48:12 +0700

    Step 15.3: install axios, load events from mock server, log to console

---
+++ src/views/EventListView.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import EventCard from '@/components/EventCard.vue'
+import type { Event } from '@/types'
+import { ref, onMounted } from 'vue'
+import axios from 'axios'
+
+const events = ref<Event[] | null>(null)
+
+onMounted(() => {
+  axios
+    .get('https://my-json-server.typicode.com/YGY-t/lab2-mock-server/events')
+    .then((response) => {
+      console.log(response.data)
+    })
+    .catch((error) => {
+      console.error('There was an error!', error)
+    })
+})
+</script>
+
+<template>
+  <h1>Events For Good</h1>
+  <div class="events">
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
+  </div>
+</template>
+
+<style scoped>
+.events {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
